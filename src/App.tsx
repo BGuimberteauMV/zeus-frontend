5ac627f4854a28936a3c75c8e595702673f8cdd3
Benjamin Guimberteau <b.guimberteau@mesvaccins.net>
2023-02-03 12:44:18 +0100

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,113 +1,112 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { BoltIcon } from '@heroicons/react/24/solid'
-import './App.css'
-import Application from './components/Application'
-import appSciences from './assets/images/app-sciences.jpg'
-import appNews from './assets/images/app-news.jpg'
-import appDocs from './assets/images/app-docs.jpg'
-import appFaq from './assets/images/app-faq.jpg'
-import appBooks from './assets/images/app-books.jpg'
-import appBills from './assets/images/app-bills.jpg'
-import appDirectory from './assets/images/app-directory.jpg'
-import appSocial from './assets/images/app-social.jpg'
-import appEat from './assets/images/app-eat.jpg'
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { BoltIcon } from "@heroicons/react/24/solid";
+import "./App.css";
+import Application from "./components/Application";
+import appSciences from "./assets/images/app-sciences.jpg";
+import appNews from "./assets/images/app-news.jpg";
+import appDocs from "./assets/images/app-docs.jpg";
+import appFaq from "./assets/images/app-faq.jpg";
+import appBooks from "./assets/images/app-books.jpg";
+import appBills from "./assets/images/app-bills.jpg";
+import appDirectory from "./assets/images/app-directory.jpg";
+import appSocial from "./assets/images/app-social.jpg";
+import appEat from "./assets/images/app-eat.jpg";
 
 const user = {
-  name: 'Benjamin Guimberteau',
-  email: 'b.guimberteau@mesvaccins.net',
+  name: "Benjamin Guimberteau",
+  email: "b.guimberteau@mesvaccins.net",
   imageUrl:
-    'https://images.unsplash.com/photo-1615807713086-bfc4975801d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&h=256&q=80',
-}
+    "https://images.unsplash.com/photo-1615807713086-bfc4975801d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&h=256&q=80",
+};
 
-const navigation = [
-  { name: 'Applications', href: '#', current: true }
-]
+const navigation = [{ name: "Applications", href: "#", current: true }];
 
 const userNavigation = [
-  { name: 'Votre profil', href: '#' },
-  { name: 'Configurations', href: '#' },
-  { name: 'Déconnexion', href: '#' },
-]
+  { name: "Votre profil", href: "#" },
+  { name: "Configurations", href: "#" },
+  { name: "Déconnexion", href: "#" },
+];
 
 const applications = [
   {
     id: 0,
-    name: 'Scientifiques',
-    description: 'Enrichissement des informations pour les maladies, vaccins, composants et autres domaine scientifique',
+    name: "Scientifiques",
+    description:
+      "Enrichissement des informations pour les maladies, vaccins, composants et autres domaine scientifique",
     imageSrc: appSciences,
-    href: '#',
-    last_updated_at: 'Mise à jour il y a 3 heures'
+    href: "#",
+    last_updated_at: "Mise à jour il y a 3 heures",
   },
   {
     id: 1,
-    name: 'Nouvelles',
-    description: 'Rédaction des nouvelles pour le grand public, les professionels de santé ou les deux',
+    name: "Nouvelles",
+    description: "Rédaction des nouvelles pour le grand public, les professionels de santé ou les deux",
     imageSrc: appNews,
-    href: '#',
-    last_updated_at: 'Mise à jour il y a 2 jours'
+    href: "#",
+    last_updated_at: "Mise à jour il y a 2 jours",
   },
   {
     id: 2,
-    name: 'Documentations',
-    description: 'Rédaction des documentations pour le grand public, les professionels de santé ou les deux',
+    name: "Documentations",
+    description: "Rédaction des documentations pour le grand public, les professionels de santé ou les deux",
     imageSrc: appDocs,
-    href: '#',
-    last_updated_at: 'Mise à jour il y a 5 jours'
+    href: "#",
+    last_updated_at: "Mise à jour il y a 5 jours",
   },
   {
     id: 3,
-    name: 'Questions fréquentes',
-    description: 'Rédaction des questions fréquentes pour le grand public, les professionels de santé ou les deux',
+    name: "Questions fréquentes",
+    description: "Rédaction des questions fréquentes pour le grand public, les professionels de santé ou les deux",
     imageSrc: appFaq,
-    href: '#',
-    last_updated_at: 'Mise à jour il y a 10 jours'
+    href: "#",
+    last_updated_at: "Mise à jour il y a 10 jours",
   },
   {
     id: 4,
-    name: 'GED',
-    description: 'Gestion Électronique des Documents pour les règles du SAD, les vaccins, les maladies et le site web',
+    name: "GED",
+    description: "Gestion Électronique des Documents pour les règles du SAD, les vaccins, les maladies et le site web",
     imageSrc: appBooks,
-    href: '#',
-    last_updated_at: 'Mise à jour il y a 10 jours'
+    href: "#",
+    last_updated_at: "Mise à jour il y a 10 jours",
   },
   {
     id: 5,
-    name: 'Facturation',
-    description: 'Gestion des factures, clients et les paiements',
+    name: "Facturation",
+    description: "Gestion des factures, clients et les paiements",
     imageSrc: appBills,
-    href: '#',
-    last_updated_at: 'Mise à jour il y a 10 jours'
+    href: "#",
+    last_updated_at: "Mise à jour il y a 10 jours",
   },
   {
     id: 6,
-    name: 'Annuaire des professionnels de santé',
+    name: "Annuaire des professionnels de santé",
     description: "Gestion de l'annuaire des professionels de santé",
     imageSrc: appDirectory,
-    href: '#',
-    last_updated_at: 'Mise à jour il y a 10 jours'
+    href: "#",
+    last_updated_at: "Mise à jour il y a 10 jours",
   },
   {
     id: 7,
-    name: 'Réseaux Sociaux',
-    description: 'Interractions sur les réseaux sociaux',
+    name: "Réseaux Sociaux",
+    description: "Interractions sur les réseaux sociaux",
     imageSrc: appSocial,
-    href: '#',
-    last_updated_at: 'Mise à jour il y a 10 jours'
+    href: "#",
+    last_updated_at: "Mise à jour il y a 10 jours",
   },
   {
     id: 8,
     name: "On mange où aujourd'hui ?",
     description: "Choix d'un lieu de restauration pour le midi",
     imageSrc: appEat,
-    href: '#',
-    last_updated_at: 'Mise à jour il y a 10 jours'
-  }
-]
+    href: "#",
+    last_updated_at: "Mise à jour il y a 10 jours",
+  },
+];
 
-function classNames(...classes:string[]) {
-  return classes.filter(Boolean).join(' ')
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
 }
 
 function App() {
@@ -132,11 +131,11 @@ function App() {
                           href={item.href}
                           className={classNames(
                             item.current
-                              ? 'border-indigo-500 text-gray-900'
-                              : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                            'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                              ? "border-indigo-500 text-gray-900"
+                              : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                            "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                           )}
-                          aria-current={item.current ? 'page' : undefined}
+                          aria-current={item.current ? "page" : undefined}
                         >
                           {item.name}
                         </a>
@@ -176,8 +175,8 @@ function App() {
                                 <a
                                   href={item.href}
                                   className={classNames(
-                                    active ? 'bg-gray-100' : '',
-                                    'block px-4 py-2 text-sm text-gray-700'
+                                    active ? "bg-gray-100" : "",
+                                    "block px-4 py-2 text-sm text-gray-700"
                                   )}
                                 >
                                   {item.name}
@@ -212,11 +211,11 @@ function App() {
                       href={item.href}
                       className={classNames(
                         item.current
-                          ? 'bg-indigo-50 border-indigo-500 text-indigo-700'
-                          : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
-                        'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                          ? "bg-indigo-50 border-indigo-500 text-indigo-700"
+                          : "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800",
+                        "block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
                       )}
-                      aria-current={item.current ? 'page' : undefined}
+                      aria-current={item.current ? "page" : undefined}
                     >
                       {item.name}
                     </Disclosure.Button>
@@ -271,7 +270,8 @@ function App() {
                   {applications.map((application) => (
                     <li
                       key={application.id}
-                      className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
+                      className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
+                    >
                       <Application application={application} />
                     </li>
                   ))}
@@ -283,7 +283,7 @@ function App() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
