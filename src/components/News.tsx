33ad6d7b57ecbae2b7500e41ade6b3d1a4975f9b
Benjamin Guimberteau <b.guimberteau@mesvaccins.net>
2023-02-21 16:09:40 +0100

    remove unnecessary files

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -60,15 +60,15 @@ function News() {
           <div className="space-y-8 divide-y divide-gray-200">
             <div className="grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
               <div className="sm:col-span-6">
-                <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
+                <label htmlFor="title" className="block text-sm font-medium text-gray-700">
                   Titre
                 </label>
                 <div className="mt-1">
                   <input
                     type="text"
-                    name="street-address"
-                    id="street-address"
-                    autoComplete="street-address"
+                    name="title"
+                    id="title"
+                    autoComplete="title"
                     className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   />
                 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      tsConfigFilePath: 'tsconfig.build.json',
-    }),
-  ],
+  plugins: [react()],
 })
